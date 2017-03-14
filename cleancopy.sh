78cf6765b5efa87c498dc4c315dc95eaa82dc113
Dermot Daly <dermot.daly@tapadoo.com>
2017-03-14 10:48:04 +0000

    Added in ability to strip commas

--- cleancopy.sh
+++ cleancopy.sh
@@ -1,2 +1,2 @@
 # Simple shell script to transform a tab delimited file of credit card transaction taken from AIB Business Banking into a CSV file that can be imported into Freeagent
-cat $1 | sed -e 's/^ //' | awk -F '\t ' '{ if ($3 == "") {print $1","$4","$2} else {print $1",-"$3","$2}}'
+cat $1 | sed 's/,//g' | sed -e 's/^ //' | awk -F '\t ' '{ if ($3 == "") {print $1","$4","$2} else {print $1",-"$3","$2}}'
